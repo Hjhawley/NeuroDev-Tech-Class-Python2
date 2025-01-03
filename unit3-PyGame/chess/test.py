56feb80b780bf1c1500ed5e282274178f91b6765
Hayden <hjhawley7@gmail.com>
2025-01-03 08:41:34 -0700

    Updated chess pieces

--- unit3-PyGame/chess/test.py
+++
@@ -1,9 +0,0 @@
-import imp
-from PIL import Image
-from piece_dict import piece_icons
-
-from PIL import Image
-im = Image.open(piece_icons["queen"], 'r')
-width, height = im.size
-pixel_values = list(im.getdata())
-print(pixel_values)
